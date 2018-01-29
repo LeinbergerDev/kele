998918f301236b0e1c8de9bdd2afaf06eeef15ea
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-01-29 12:50:57 -0600

    checkpoint-submission

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -17,4 +17,9 @@ module Roadmap
 
     @checkpoint = JSON.parse(response.body)
   end
+# k.create_checkpoint_submission("")
+  def create_checkpoint_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", values: {assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id}, headers: { "authorization" => @auth_token } )
+    @submission = JSON.parse(response.body)
+  end
 end
